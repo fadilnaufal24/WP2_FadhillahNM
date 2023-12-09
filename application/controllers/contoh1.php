1bd23eed346432d36f972d26dfee493ecc6300c1
fadilnaufal24 <brizialexander64@gmail.com>
2023-12-09 16:30:37 +0700

    menambahkan Web Programming 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama: Fadhillah Naufal Muhammad
+             Nim: 19221082
+             kelas: 19.3A.26
+            Saya tinggal di daerah Kebon Jeruk
+            olahraga yang saya sukai adalah
+            Basket";
+    }
+}
